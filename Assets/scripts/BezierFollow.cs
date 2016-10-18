68e3e1986a0fd247fcdb91c99ee78645c515f249
HaoHuaqing <534390598@qq.com>
2016-10-18 19:02:59 +0800

    Initial commit

---
+++ Assets/scripts/BezierFollow.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(LineRenderer))]
+public class BezierFollow : MonoBehaviour
+{
+
+    public LineRenderer lineRenderer;
+    public GameObject[] CreatPoint;
+    public Transform[] controlPoints;
+    public Transform startPosition;
+    public GameObject RandomPoint;
+    Vector3 uppoint = new Vector3(0, 10, 0);
+    private int curveCount = 0;
+    private int layerOrder = -1;
+    private int SEGMENT_COUNT = 100;
+    private bool readpoint = true;
+
+    void Start()
+    {
+        if (!lineRenderer)
+        {
+            lineRenderer = GetComponent<LineRenderer>();
+        }
+        lineRenderer.sortingLayerID = layerOrder;
+        curveCount = (int)controlPoints.Length / 2;//draw line every 3 points.0,1,2;2,3,4;
+
+
+    }
+    void Update()
+    {
+        if (readpoint)
+        {
+            for (int i = 1; i < 99; i = i + 2)
+            {
+                Vector3 RandX = new Vector3(Bezier.Rand[i], 0, 0);
+                //Debug.Log(Bezier.Rand[i]);
+                CreatPoint[i] = Instantiate(RandomPoint, startPosition.transform.position + i * uppoint + RandX, Quaternion.identity) as GameObject;
+                CreatPoint[i + 1] = Instantiate(RandomPoint, startPosition.transform.position + (i + 1) * uppoint, Quaternion.identity) as GameObject;
+                controlPoints[i] = CreatPoint[i].transform;
+                controlPoints[i + 1] = CreatPoint[i + 1].transform;
+            }
+            readpoint = false;
+        }
+
+        DrawCurve();
+    }
+    void DrawCurve()
+    {
+
+        for (int j = 0; j < curveCount; j++)
+        {
+            for (int i = 1; i <= SEGMENT_COUNT; i++)
+            {
+                float t = i / (float)SEGMENT_COUNT;
+                int nodeIndex = j * 2;
+                Vector3 pixel = CalculateCubicBezierPoint(t, controlPoints[nodeIndex].position, controlPoints[nodeIndex + 1].position, controlPoints[nodeIndex + 2].position);
+                lineRenderer.SetVertexCount(((j * SEGMENT_COUNT) + i));
+                lineRenderer.SetPosition((j * SEGMENT_COUNT) + (i - 1), pixel);
+            }
+
+        }
+    }
+
+    Vector3 CalculateCubicBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+
+        Vector3 p = uu * p0;
+        p += 2 * u * t * p1;
+        p += tt * p2;
+
+        return p;
+    }
+
+}
---
+++ Assets/scripts/carcontrol.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class carcontrol : MonoBehaviour
+{
+    static List<string> mWriteTxt = new List<string>();
+    private string outpath;
+    public Text timetext;
+    private float timer = 0f;
+    private int time = 0;
+    private int pixelcount = 25;//(car.y-startpostion.y)/0.2
+    private float carmoveforward = 0;
+    private float randNormal = 0f;
+    private float mean = 0f;
+    private float stdDev = 0f;
+    private float stdDev1 = 0f;
+    private float stdDev2 = 0f;
+    private float stdDev3 = 0f;
+    private float stdDev4 = 0f;
+    private float stdDev5 = 0f;
+    private float carspeedY = 0f;
+
+
+    public void Awake()
+    {
+        //定时器
+        InvokeRepeating("LaunchProjectile", 0, 0.1F);  //0秒后，每0.1f调用一次
+
+    }
+    void LaunchProjectile()
+    {
+        //产生高斯扰动
+
+        System.Random rand = new System.Random(); //reuse this if you are generating many
+        double u1 = rand.NextDouble(); //these are uniform(0,1) random doubles
+        double u2 = rand.NextDouble();
+        double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) *
+                     Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+        randNormal = (float)
+                     (mean + stdDev * randStdNormal); //random normal(mean,stdDev^2)
+    }
+    void Start()
+    {
+        string filename = "";
+        CSVHelper.Instance().ReadCSVFile("configuration", (table) => {
+
+            // 可以遍历整张表
+            foreach (CSVLine line in table)
+            {
+                foreach (KeyValuePair<string, string> item in line)
+                {
+                    Debug.Log(string.Format("item key = {0} item value = {1}", item.Key, item.Value));
+                }
+            }
+            //可以拿到表中任意一项数据
+            filename = table["1"]["name"];
+            //Debug.Log(filename);
+            outpath = "D:\\testDir\\" + filename + ".csv";
+            stdDev1 = float.Parse(table["2"]["name"]);
+            stdDev2 = float.Parse(table["3"]["name"]);
+            stdDev3 = float.Parse(table["4"]["name"]);
+            stdDev4 = float.Parse(table["5"]["name"]);
+            stdDev5 = float.Parse(table["6"]["name"]);
+            carspeedY= float.Parse(table["7"]["name"]);
+        });
+
+
+        //每次启动客户端删除之前保存的Log  
+        if (File.Exists(outpath))
+        {
+            File.Delete(outpath);
+        }
+    }
+
+    void Update()
+    {
+        float JoystickY = Input.GetAxis("Horizontal");
+        Vector3 move = new Vector3(10 * (JoystickY+randNormal), carmoveforward, 0);
+        transform.position = move;
+        //Debug.Log(move);
+        if (move.x <= (Bezier.pixel[pixelcount].x + 5) && move.x >= (Bezier.pixel[pixelcount].x + 0.5))
+        {
+            carmoveforward += carspeedY;
+            pixelcount++;
+        }
+        else if (move.x <= (Bezier.pixel[pixelcount].x - 0.5) && move.x >= (Bezier.pixel[pixelcount].x - 5))
+        {
+            carmoveforward += carspeedY;
+            pixelcount++;
+        }
+
+        if (transform.position.y >= 0 && transform.position.y <= 190)
+        {
+            stdDev = stdDev1;
+        }
+        else if (transform.position.y >= 190 && transform.position.y <= 380)
+        {
+            stdDev = stdDev2;
+        }
+        else  if(transform.position.y >= 380 && transform.position.y <= 570)
+        {
+            stdDev = stdDev3;
+        }
+        else if (transform.position.y >= 570 && transform.position.y <= 760)
+        {
+            stdDev = stdDev4;
+        }
+        else if (transform.position.y >= 760 && transform.position.y <= 950)
+        {
+            stdDev = stdDev5;
+        }
+
+        
+        timer = Time.time;
+        //Debug.Log(h);
+        time = (int)timer;
+        timetext.text = ("Time:" + time);
+
+        string[] temp = { randNormal.ToString(), ",", transform.position.x.ToString(), ",", Bezier.pixel[pixelcount-1].y.ToString(), "\r\n" };
+        foreach (string t in temp)
+        {
+            using (StreamWriter writer = new StreamWriter(outpath, true, Encoding.UTF8))
+            {
+                writer.Write(t);
+            }
+            mWriteTxt.Remove(t);
+        }
+        
+    }
+}
